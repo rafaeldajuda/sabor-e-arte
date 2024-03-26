52d964af82233ef2e046f1a2171b6724ee1f2c9a
Rafael D Ajuda <rafael.ajuda@engdb.com.br>
2024-03-26 16:11:11 -0300

    add opcoes

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"log"
 	"os"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
@@ -14,6 +15,24 @@ var menu = map[string]string{
 	"4": "Minhas reservas",
 	"5": "Cancelar reserva",
 }
+
+var menuKeyboard = tgbotapi.NewReplyKeyboard(
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("1. Menu"),
+	),
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("2. Ver mesas disponíveis"),
+	),
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("3. Fazer reserva"),
+	),
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("4. Minhas reservas"),
+	),
+	tgbotapi.NewKeyboardButtonRow(
+		tgbotapi.NewKeyboardButton("5. Cancelar reserva"),
+	),
+)
 
 func main() {
 	godotenv.Load()
@@ -30,16 +49,38 @@ func main() {
 	// Start polling Telegram for updates.
 	updates := bot.GetUpdatesChan(updateConfig)
 
+	// for update := range updates {
+	// 	if update.Message == nil {
+	// 		continue
+	// 	}
+
+	// 	msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+	// 	msg.ReplyToMessageID = update.Message.MessageID
+
+	// 	if _, err := bot.Send(msg); err != nil {
+	// 		panic(err)
+	// 	}
+	// }
+
 	for update := range updates {
-		if update.Message == nil {
+		if update.Message == nil { // ignore non-Message updates
 			continue
 		}
 
 		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
-		msg.ReplyToMessageID = update.Message.MessageID
+		t := tgbotapi.BotCommand{Command: "start", Description: "Seja bem vindo ao restaurante Sabor e Arte. Para ver as opções digite open"}
+
+		switch update.Message.Text {
+		case "/start":
+			msg.Text = t.Description
+		case "open":
+			msg.ReplyMarkup = menuKeyboard
+		case "close":
+			msg.ReplyMarkup = tgbotapi.NewRemoveKeyboard(true)
+		}
 
 		if _, err := bot.Send(msg); err != nil {
-			panic(err)
+			log.Panic(err)
 		}
 	}
 
