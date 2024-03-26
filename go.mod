bbf4d99d1adb9f74f18bc3a3a6b41a8392b80fe2
Rafael D Ajuda <rafael.ajuda@engdb.com.br>
2024-03-26 14:53:55 -0300

    telegram api

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/rafaeldajuda/sabor-e-arte-golang-telegram
+
+go 1.22.0
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+	github.com/joho/godotenv v1.5.1
+)
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
+)
+
+var menu = map[string]string{
+	"1": "Menu",
+	"2": "Ver mesas disponíveis",
+	"3": "Fazer reserva",
+	"4": "Minhas reservas",
+	"5": "Cancelar reserva",
+}
+
+func main() {
+	godotenv.Load()
+
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TELEGRAM_TOKEN"))
+	if err != nil {
+		panic(err)
+	}
+
+	bot.Debug = true
+	updateConfig := tgbotapi.NewUpdate(0)
+	updateConfig.Timeout = 30
+
+	// Start polling Telegram for updates.
+	updates := bot.GetUpdatesChan(updateConfig)
+
+	for update := range updates {
+		if update.Message == nil {
+			continue
+		}
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		if _, err := bot.Send(msg); err != nil {
+			panic(err)
+		}
+	}
+
+}
